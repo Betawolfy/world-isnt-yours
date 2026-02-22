e426023c39090f7334b03e35b45113808432bbbc
Betawolfy <betawolfy@proton.me>
2026-02-22 21:54:26 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,38 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeObsidian from 'starlight-theme-obsidian'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [Icons({ compiler: 'astro' })],
+	},
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Dist Err SMP Wiki',
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Getting started', slug: 'guides/getting-started' },
+						{ label: 'Origins', slug: 'guides/origins' },
 					],
 				},
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				{
+					label: 'Misc',
+					items: [
+						{ label: 'Credits', slug: 'credits' },
+					]
+				},
 			],
+			plugins: [starlightThemeObsidian()],
 		}),
 	],
 });
